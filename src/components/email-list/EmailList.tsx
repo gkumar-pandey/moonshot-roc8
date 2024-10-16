62b52b9d8ca77630e65cb20630982ffa3f44857e
gkumar-pandey <gautamshekhar078@gmail.com>
2024-10-16 21:56:47 +0530

    email list component created

---
+++ src/components/email-list/EmailList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EmailCard from "../email-card/EmailCard";
+
+const EmailList = () => {
+  return (
+    <div>
+      <EmailCard />
+    </div>
+  );
+};
+
+export default EmailList;
