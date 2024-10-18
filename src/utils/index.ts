815f82efcc8a50d9764a8195c76556f926ba6596
gkumar-pandey <gautamshekhar078@gmail.com>
2024-10-19 02:34:03 +0530

    Added comment to utils functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,23 +1,29 @@
 import { FilterEmailsType } from "../types/EmailTypes";
 
+/**
+ * @param timestamp : Date of the email
+ * @returns : Formated Date DD/MM/YY/ hh:mm
+ */
 export const formatTimestamp = (timestamp: number | undefined) => {
   const date = new Date(Number(timestamp));
-
-  // Extract date components
   const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = String(date.getFullYear());
   let hours = date.getHours();
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12; // Convert 0 or 24 to 12 for midnight and handle 12-hour format
+  hours = hours % 12 || 12;
 
-  // Construct the final formatted date
   return `${day}/${month}/${year} ${hours}:${minutes} ${ampm}`;
 };
 
 export const API_URL = "https://flipkart-email-mock.now.sh/";
 
+/**
+ * @param emails : Array of emails
+ * @param filterBy : Filter types like Read, Unread, Favorite & all
+ * @returns : Array of Filtered Array
+ */
 export const filterEmails: FilterEmailsType = (emails, filterBy) => {
   if (filterBy === "Unread") {
     return emails?.filter((ele, idx) => !ele?.isRead);
@@ -34,12 +40,18 @@ export const filterEmails: FilterEmailsType = (emails, filterBy) => {
   return emails;
 };
 
-// Store Updated Emails to LocalStorage
+/**
+ * @param key : localstorage stored data key
+ * @param data : updated Emails Data
+ */
 export const storeDataToLocalStorage = (key: string, data: unknown) => {
   localStorage.setItem(key, JSON.stringify(data));
 };
 
-// Retrieve Stored emails from localstorage
+/**
+ * @param key : Localstorage stored data key
+ * @returns : Array of stored data or and empty Array
+ */
 export const getDataFromLocalStorage = (key: string) => {
   const data = localStorage.getItem(key);
   return (data && JSON.parse(data)) || [];
